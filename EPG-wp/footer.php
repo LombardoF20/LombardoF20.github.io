dee416522d918f1f7684e83093260114c9c61949
Lombardo <50172153+LombardoF20@users.noreply.github.com>
2020-03-17 09:41:46 +0200

    epg

---
+++ EPG-wp/footer.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package EPG
+ */
+
+?>
+
+<footer class="container-fluid footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-8 footer-block-logo">
+				<img src="/wp-content/themes/epg-theme/img/logo.png" alt="">
+				<p>Виробництво і <br>
+	поставка <br>
+	котельного обладнання</p>
+			</div>
+			<div class="col-4  footer-block-number">
+					<a class="num" href="tel:380986049109">38 (098) 60-49-109</a>
+					<a class="btn" href="#">Перетелефонувати вам?</a>
+			</div>
+		</div>
+	</div>
+	<div class="block-footer-end">
+		<div class="container">
+			<div class="row">
+				<div class="col-6 footer-end-left">
+					<p>(с) 2020 effect-m.com Всі права захищені</p>
+				</div>
+				<div class="col-6 footer-end-right">
+					<a href="#">Політика конфіденційності</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- <div class="popup-konsultation1">
+		<h4>Отримати консультацію</h4>
+		<input type="text" placeholder="Як до Вас звертатися?*">
+		<input type="text" placeholder="Номер телефону*">
+		<input type="text" placeholder="Назва компанії">
+		<input type="text" placeholder="Електронна пошта*">
+		<button type="button" name="button">Залишити заявку</button>
+		<p>Ми обов’язково зв'яжемося з вами протягом робочого дня</p>
+		<span>(+ Ви отримаєте наш електронний каталог продукції)</span>
+	</div> -->
+	<!-- <div class="popup-konsultation2">
+		<h4>Обрати котел правильно не легка справа. Наші спеціалісти допоможуть Вам прийняти правильне рішення</h4>
+		<p class="mt">Залиште свої контакти і з Вами зв’яжуться в найближчий час</p>
+		<input type="text" placeholder="Номер телефону*">
+		<button type="button" name="button">Залишити заявку</button>
+	</div> -->
+	<!-- <div class="popup-special">
+		<h4>Отримати консультацію</h4>
+		<input type="text" placeholder="Як до Вас звертатися?*">
+		<input type="text" placeholder="Номер телефону*">
+		<input type="text" placeholder="Назва компанії">
+		<input type="text" placeholder="Електронна пошта*">
+		<button type="button" name="button">Залишити заявку</button>
+		<p>Ми обов’язково зв'яжемося з вами протягом робочого дня</p>
+	</div> -->
+</footer>
+
+<!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/js/swiper.min.js"></script>
+
+<script>
+
+</script>
+
+<script type="text/javascript" src="/wp-content/themes/epg-theme/js/vendors.e3d7605c4805af7dc233.js"></script><script type="text/javascript" src="/wp-content/themes/epg-theme/js/app.4308d4e6739cca9af606.js"></script>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ EPG-wp/page-mainUA.php
@@ -0,0 +1,439 @@
+<?php
+/*
+	 Template Name: main
+	 */
+get_header(); ?>
+
+<div class="container block-slider-uslygi">
+	<div class="block-slider-uslygi-head">
+		<h2><span> Послуги </span> компанії</h2>
+		<p>Чим ми <span> займаємося</span></p>
+	</div>
+	<div class="block-block-slider-uslygi-main">
+		<div class="row">
+			<div class="col-1">
+				<!-- Add Arrows -->
+				<div class="swiper-button-prev"></div>
+			</div>
+			<div class="col-10">
+				<div class="swiper-container">
+					<div class="swiper-wrapper">
+						<div class="swiper-slide swiper-slide-uslygi">
+								<div class="swiper-slide-uslygi-wrapper">
+									<img src="/wp-content/themes/epg-theme/img/img SERVICE.jpg" alt="">
+									<div class="swiper-slide-uslygi-bg"></div>
+									<h4>Виробництво і продаж котлів</h4>
+									<ul>
+										<li>- власна торгова марка</li>
+										<li>- власна торгова марка котлів</li>
+										<li>- імпорт з "перших рук"</li>
+									</ul>
+								</div>
+								<a href="#">Детальніше</a>
+						</div>
+						<div class="swiper-slide swiper-slide-uslygi">
+							<div class="swiper-slide-uslygi-wrapper">
+								<img src="/wp-content/themes/epg-theme/img/01  shape.jpg" alt="">
+								<div class="swiper-slide-uslygi-bg"></div>
+								<h4>Виробництво і монтаж котелень</h4>
+								<ul>
+									<li>- власна торгова марка</li>
+									<li>- власна торгова марка котлів</li>
+									<li>- імпорт з "перших рук"</li>
+								</ul>
+							</div>
+							<a href="#">Детальніше</a>
+						</div>
+						<div class="swiper-slide swiper-slide-uslygi">
+							<div class="swiper-slide-uslygi-wrapper">
+								<img src="/wp-content/themes/epg-theme/img/01  shape(1).jpg" alt="">
+								<div class="swiper-slide-uslygi-bg"></div>
+								<h4>Поставка енергоносіїв</h4>
+								<ul>
+									<li>- власна торгова марка</li>
+									<li>- власна торгова марка котлів</li>
+									<li>- імпорт з "перших рук"</li>
+								</ul>
+							</div>
+							<a href="#">Детальніше</a>
+						</div>
+						<div class="swiper-slide swiper-slide-uslygi">
+								<div class="swiper-slide-uslygi-wrapper">
+									<img src="/wp-content/themes/epg-theme/img/02  shape.jpg" alt="">
+									<div class="swiper-slide-uslygi-bg"></div>
+									<h4>Газопроводи, інженерні мережі. Проєкти "під ключ"</h4>
+									<ul>
+										<li>- власна торгова марка</li>
+										<li>- власна торгова марка котлів</li>
+										<li>- імпорт з "перших рук"</li>
+									</ul>
+								</div>
+								<a href="#">Детальніше</a>
+						</div>
+						<div class="swiper-slide swiper-slide-uslygi">
+							<div class="swiper-slide-uslygi-wrapper">
+								<img src="/wp-content/themes/epg-theme/img/img SERVICE.jpg" alt="">
+								<div class="swiper-slide-uslygi-bg"></div>
+								<h4>Виробництво і продаж котлів</h4>
+								<ul>
+									<li>- власна торгова марка</li>
+									<li>- власна торгова марка котлів</li>
+									<li>- імпорт з "перших рук"</li>
+								</ul>
+							</div>
+							<a href="#">Детальніше</a>
+						</div>
+					</div>
+
+				</div>
+			</div>
+			<div class="col-1">
+				<!-- Add Arrows -->
+				<div class="swiper-button-next"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container block-slider-object">
+	<div class="block-slider-object-head">
+		<h2> Наші <span>об’єкти</span></h2>
+		<p>Ми<span> виробник</span></p>
+	</div>
+	<div class="block-slider-object-main">
+		<div class="row">
+			<div class="col-1">
+				<!-- Add Arrows -->
+				<div class="swiper-button-prev-object"></div>
+			</div>
+			<div class="col-10">
+				<div class="swiper-container-object">
+					<div class="swiper-wrapper">
+						<div class="swiper-slide swiper-slide-object">
+							<img src="/wp-content/themes/epg-theme/img/case_img.jpg" alt="">
+								<div class="swiper-slide-object-wrapper">
+									<h4>Газова водогрійна котельня 3,5 Вт</h4>
+									<p class="object-wrapper-p1">ТОВ “Променерго”</p>
+									<p class="object-wrapper-p2">Площа об'єкта: <br>Термін реалізації: <br>Потужність: <br>Завдання:<span>105 кв.м. <br>34 дня <br>105 кв.м.</span></p>
+									<p class="object-wrapper-3">Промислові котли та котельні на пелетах EUROTHERM WMSP</p>
+								</div>
+						</div>
+						<div class="swiper-slide swiper-slide-object">
+							<img src="/wp-content/themes/epg-theme/img/case_img2.jpg" alt="">
+								<div class="swiper-slide-object-wrapper">
+									<h4>Газова водогрійна котельня 3,5 Вт</h4>
+									<p class="object-wrapper-p1">ТОВ “Променерго”</p>
+									<p class="object-wrapper-p2">Площа об'єкта: <br>Термін реалізації: <br>Потужність: <br>Завдання:<span>105 кв.м. <br>34 дня <br>105 кв.м.</span></p>
+									<p class="object-wrapper-3">Промислові котли та котельні на пелетах EUROTHERM WMSP</p>
+								</div>
+						</div>
+						<div class="swiper-slide swiper-slide-object">
+							<img src="/wp-content/themes/epg-theme/img/case_img3.jpg" alt="">
+								<div class="swiper-slide-object-wrapper">
+									<h4>Газова водогрійна котельня 3,5 Вт</h4>
+									<p class="object-wrapper-p1">ТОВ “Променерго”</p>
+									<p class="object-wrapper-p2">Площа об'єкта: <br>Термін реалізації: <br>Потужність: <br>Завдання:<span>105 кв.м. <br>34 дня <br>105 кв.м.</span></p>
+									<p class="object-wrapper-3">Промислові котли та котельні на пелетах EUROTHERM WMSP</p>
+								</div>
+						</div>
+						<div class="swiper-slide swiper-slide-object">
+							<img src="/wp-content/themes/epg-theme/img/case_img3.jpg" alt="">
+								<div class="swiper-slide-object-wrapper">
+									<h4>Газова водогрійна котельня 3,5 Вт</h4>
+									<p class="object-wrapper-p1">ТОВ “Променерго”</p>
+									<p class="object-wrapper-p2">Площа об'єкта: <br>Термін реалізації: <br>Потужність: <br>Завдання:<span>105 кв.м. <br>34 дня <br>105 кв.м.</span></p>
+									<p class="object-wrapper-3">Промислові котли та котельні на пелетах EUROTHERM WMSP</p>
+								</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-1">
+				<!-- Add Arrows -->
+				<div class="swiper-button-next-object"></div>
+			</div>
+			<div class="row block-slider-object-main-a">
+				<a href="#">Переглянути всі кейси</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container block-slider-catalog">
+	<div class="block-slider-catalog-head">
+		<h2> Асортимент <span> котлів</span></h2>
+		<p>Каталог <span> товарів</span></p>
+	</div>
+	<div class="block-slider-catalog-main">
+		<div class="row">
+			<div class="col-1">
+				<!-- Add Arrows -->
+				<div class="swiper-button-prev-catalog"></div>
+			</div>
+			<div class="col-10">
+				<div class="swiper-container-catalog">
+					<div class="swiper-wrapper">
+						<div class="swiper-slide swiper-slide-catalog">
+							<h3 color="blue">Новинка</h3>
+							<img class="swiper-slide-catalog-headImg" src="/wp-content/themes/epg-theme/img/product_img.jpg" alt="">
+								<div class="swiper-slide-catalog-wrapper">
+									<h4>Газова водогрійна котельня 3,5 Вт</h4>
+									<div class="swiper-slide-catalog-wrapper-btns">
+										<a class="btns-more" href="#">Детально</a>
+										<a class="btns-call" href="#"><img src="/wp-content/themes/epg-theme/img/phone.svg" alt=""></a>
+									</div>
+								</div>
+						</div>
+						<div class="swiper-slide swiper-slide-catalog">
+							<h3 color="red">Прямі поставки</h3>
+							<img class="swiper-slide-catalog-headImg" src="/wp-content/themes/epg-theme/img/product_img2.jpg" alt="">
+								<div class="swiper-slide-catalog-wrapper">
+									<h4>Промисловий котел на пеллетах та з автоматичною подачею Eurotherm WMSP </h4>
+									<div class="swiper-slide-catalog-wrapper-btns">
+										<a class="btns-more" href="#">Детально</a>
+										<a class="btns-call" href="#"><img src="/wp-content/themes/epg-theme/img/phone.svg" alt=""></a>
+									</div>
+								</div>
+						</div>
+						<div class="swiper-slide swiper-slide-catalog">
+							<h3 color="dark-blue">Високий ККД</h3>
+							<img class="swiper-slide-catalog-headImg" src="/wp-content/themes/epg-theme/img/product_img3.jpg" alt="">
+								<div class="swiper-slide-catalog-wrapper">
+									<h4>Газовий паровий котел IVAR BLP 500 кг пара в годину (від 0,98 БАР)</h4>
+									<div class="swiper-slide-catalog-wrapper-btns">
+										<a class="btns-more" href="#">Детально</a>
+										<a class="btns-call" href="#"><img src="/wp-content/themes/epg-theme/img/phone.svg" alt=""></a>
+									</div>
+								</div>
+						</div>
+						<div class="swiper-slide swiper-slide-catalog">
+							<h3 color="blue">Новинка</h3>
+							<img class="swiper-slide-catalog-headImg" src="/wp-content/themes/epg-theme/img/product_img4.jpg" alt="">
+								<div class="swiper-slide-catalog-wrapper">
+									<h4>Промисловий газовий трьоходовий жаротрубний котел Колві 6000 Т</h4>
+									<div class="swiper-slide-catalog-wrapper-btns">
+										<a class="btns-more" href="#">Детально</a>
+										<a class="btns-call" href="#"><img src="/wp-content/themes/epg-theme/img/phone.svg" alt=""></a>
+									</div>
+								</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-1">
+				<!-- Add Arrows -->
+				<div class="swiper-button-next-catalog"></div>
+			</div>
+			<div class="row block-slider-object-main-a">
+				<a href="#">Перейти в каталог</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container block-slider-catalog">
+	<div class="block-slider-catalog-head">
+		<h2> Додаткове <span> обладнання</span></h2>
+		<p>Каталог <span> товарів</span></p>
+	</div>
+	<div class="block-slider-catalog-main">
+		<div class="row">
+			<div class="col-1">
+				<!-- Add Arrows -->
+				<div class="swiper-button-prev-complementary"></div>
+			</div>
+			<div class="col-10">
+				<div class="swiper-container-complementary">
+					<div class="swiper-wrapper">
+						<div class="swiper-slide swiper-slide-complementary">
+							<h3 color="blue">Новинка</h3>
+							<img class="swiper-slide-complementary-headImg" src="/wp-content/themes/epg-theme/img/product_img10.jpg" alt="">
+								<div class="swiper-slide-complementary-wrapper">
+									<h4>Газова водогрійна котельня 3,5 Вт</h4>
+									<div class="swiper-slide-complementary-wrapper-btns">
+										<a class="btns-more" href="#">Детально</a>
+										<a class="btns-call" href="#"><img src="/wp-content/themes/epg-theme/img/phone.svg" alt=""></a>
+									</div>
+								</div>
+						</div>
+						<div class="swiper-slide swiper-slide-complementary">
+							<h3 color="blue">Новинка</h3>
+							<img class="swiper-slide-complementary-headImg" src="/wp-content/themes/epg-theme/img/product_img11.jpg" alt="">
+								<div class="swiper-slide-complementary-wrapper">
+									<h4>Пеллетная факельная горелка KVIT LYUTA 16 КВт</h4>
+									<div class="swiper-slide-complementary-wrapper-btns">
+										<a class="btns-more" href="#">Детально</a>
+										<a class="btns-call" href="#"><img src="/wp-content/themes/epg-theme/img/phone.svg" alt=""></a>
+									</div>
+								</div>
+						</div>
+						<div class="swiper-slide swiper-slide-complementary">
+							<h3 color="blue">Новинка</h3>
+							<img class="swiper-slide-complementary-headImg" src="/wp-content/themes/epg-theme/img/product_img10.jpg" alt="">
+								<div class="swiper-slide-complementary-wrapper">
+									<h4>Пеллетная факельная горелка KVIT LYUTA 16 КВт</h4>
+									<div class="swiper-slide-complementary-wrapper-btns">
+										<a class="btns-more" href="#">Детально</a>
+										<a class="btns-call" href="#"><img src="/wp-content/themes/epg-theme/img/phone.svg" alt=""></a>
+									</div>
+								</div>
+						</div>
+						<div class="swiper-slide swiper-slide-complementary">
+							<h3 color="blue">Новинка</h3>
+							<img class="swiper-slide-complementary-headImg" src="/wp-content/themes/epg-theme/img/product_img11.jpg" alt="">
+								<div class="swiper-slide-complementary-wrapper">
+									<h4>Пеллетная факельная горелка KVIT LYUTA 16 КВт</h4>
+									<div class="swiper-slide-complementary-wrapper-btns">
+										<a class="btns-more" href="#">Детально</a>
+										<a class="btns-call" href="#"><img src="/wp-content/themes/epg-theme/img/phone.svg" alt=""></a>
+									</div>
+								</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-1">
+				<!-- Add Arrows -->
+				<div class="swiper-button-next-complementary"></div>
+			</div>
+			<div class="row block-slider-object-main-a">
+				<a href="#">Перейти в каталог</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-12 block-num1">
+			<!-- <img src="/wp-content/themes/epg-theme/img/1.svg" alt="">
+			<div class="block-num1-p">
+				<h2>Ми - <span>провідний виробник!</span></h2>
+				<p><span>Великий склад</span> асортименту в наявності</p>
+			</div> -->
+		</div>
+	</div>
+</div>
+
+<div class="container block-numAbout">
+	<div class="block-numAbout-head">
+		<h2> Цифри <span> про нас</span></h2>
+		<p><span>Про нас</span></p>
+	</div>
+	<div class="row">
+		<div class="col-12 block-numAbout-wrapper">
+			<div class="block-numAbout-item">
+				<img src="wp-content/themes/epg-theme/img//2001.svg" alt="">
+				<p>Є провідним дилером на ринку з 2001 року</p>
+			</div>
+			<div class="block-numAbout-item">
+				<img src="wp-content/themes/epg-theme/img//10.svg" alt="">
+				<p>Є провідним дилером на ринку з 2001 року</p>
+			</div>
+			<div class="block-numAbout-item">
+				<img src="wp-content/themes/epg-theme/img//3170.svg" alt="">
+				<p>Більше 3 тис. реалізованих об'єктів в портфоліо</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="col-12 block-man">
+		<p>Ми ВІДПОВІДАЄМО найскладнішим <br> тендерним вимогам!</p>
+	</div>
+</div>
+
+<div class="container block-numAbout">
+	<div class="block-numAbout-head">
+		<h2> Часто задавані <span>питання</span></h2>
+		<p><span>FAQ’s</span></p>
+	</div>
+	<div class="col-12">
+		<div class="block-qualite-FAQ">
+			<div class="faq-block" itemscope="" itemtype="https://schema.org/FAQPage" style="">
+				<div class="faq-block__item faq1-btn1" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+					<h3 class="faq-block__question" itemprop="name"><span class="h2-blue"></span>Наскільки це вигідно?<span class="faq-block__arrow1 faq-block__arrow"></span></h3>
+					<div itemscope="" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+						<div style="display:none;" class="faq-block__answer faq1-answr" itemprop="text">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+					</div>
+				</div>
+				<div class="faq-block__item faq1-btn2" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+					<h3 class="faq-block__question" itemprop="name"><span class="h2-blue"></span>Тендер - це також складно?<span class="faq-block__arrow2 faq-block__arrow"></span></h3>
+					<div itemscope="" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+						<div style="display:none;" class="faq-block__answer faq2-answr" itemprop="text">
+							Мы можем предложить оформить заказ на безглютенновый, безлактозный торт и торт с использованием сахарозаменителя. Если у вас есть еще какие-то аллергические проявления, обязатально сообщите о них менеджеру. </div>
+					</div>
+				</div>
+				<div class="faq-block__item faq1-btn3" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+					<h3 class="faq-block__question" itemprop="name"><span class="h2-blue"></span>Які є гарантії ефективності виконаної роботи?<span class="faq-block__arrow3 faq-block__arrow"></span></h3>
+					<div itemscope="" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+						<div style="display:none;" class="faq-block__answer faq3-answr" itemprop="text">
+							Для максимального комфорта желательно делать заказ заранее, но мы так же принимаем срочные заказы с отгрузкой день в день. </div>
+					</div>
+				</div>
+				<div class="faq-block__item faq1-btn4" itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question">
+					<h3 class="faq-block__question" itemprop="name"><span class="h2-blue"></span>Як формується ціна?<span class="faq-block__arrow4 faq-block__arrow"></span></h3>
+					<div itemscope="" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+						<div style="display:none;" class="faq-block__answer faq4-answr" itemprop="text">
+							Стоимость торта зависит от сложности выбранного вами декора </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid block-fos">
+	<div class="container block-numAbout">
+		<div class="block-numAbout-head">
+			<h2> Зв'яжіться <span>з нами</span></h2>
+			<p><span>Контакти</span></p>
+		</div>
+		<div class="row">
+			<div class="col-6 block-fos-items">
+				<p class="block-fos-items-p">Ви вибираєте нас, тому ми створимо всі умови для
+				<span>комфортного та взаємовигідного співробітництва</span> </p>
+				<div class="block-fos-item">
+					<img src="wp-content/themes/epg-theme/img//icon phone.svg" alt="">
+					<p>
+						Телефон
+						<a href="tel:380986049109">38 (098) 604-9109</a>
+						<a href="tel:380443920108">38 (044) 392-0108</a>
+						<a href="tel:380665248111">38 (066) 524-8111</a>
+					</p>
+				</div>
+				<div class="block-fos-item">
+					<img src="wp-content/themes/epg-theme/img//icon_mail.svg" alt="">
+					<p>
+						Пошта
+						<a href="mailto:energopromgrup@gmail.com">energopromgrup@gmail.com</a>
+					</p>
+				</div>
+				<div class="block-fos-item">
+					<img src="wp-content/themes/epg-theme/img//icon_ adress.svg" alt="">
+					<p>
+						Телефон
+						<span>
+							Україна, Київська обл., <br>
+							c. Білогородка, вул. Щаслива, 2, <br>
+							офіс 6
+						</span>
+					</p>
+				</div>
+			</div>
+			<div class="col-6 block-fos-cf7">
+				<input type="text" placeholder="Ім’я">
+				<input type="text" placeholder="Email">
+				<input type="text" placeholder="Телефон">
+				<textarea placeholder="Повідомлення" name="name"></textarea>
+				<button type="button" name="button">Відправити</button>
+			</div>
+		</div>
+</div>
+</div>
+<div class="container-fluid block-map">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d81388.43448432164!2d30.165150215764957!3d50.39659547418095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472b35548cf22d53%3A0xa7b2b10592f7f288!2z0JHRltC70L7Qs9C-0YDQvtC00LrQsCwg0JrQuNGX0LLRgdGM0LrQsCDQvtCx0Lsu!5e0!3m2!1suk!2sua!4v1583164562343!5m2!1suk!2sua" width="" height="" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+</div>
+<?php get_footer(); ?>
